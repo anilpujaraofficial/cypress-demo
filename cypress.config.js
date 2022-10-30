c0b6654de28584a82dd8600abaf1490cfbd6e0f3
Anil Pujara <anilpujara125@gmail.com>
2022-10-30 19:23:10 +0545

    update jemkins file

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,25 @@
 const { defineConfig } = require("cypress");
-
+const fs = require("fs");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("task", {
+        checkFileExists(filepath) {
+          if (fs.existsSync(filepath)) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      });
     },
 
     excludeSpecPattern: ["cypress/e2e/2-advanced-examples"],
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
     //report
     reporter: "mochawesome",
     reporterOptions: {
@@ -15,6 +28,14 @@ module.exports = defineConfig({
       overwrite: false,
       html: true,
       json: true,
+    },
+
+    //environment
+    env: {
+      testingEnv: "qa",
+      qa: {},
+      dev: {},
+      uat: {},
     },
   },
 });
