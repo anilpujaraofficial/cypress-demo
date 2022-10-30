b1468be5cf7fdaba209e71a52d9793191f2eaac6
Anil Pujara <anilpujara125@gmail.com>
2022-10-30 09:39:55 +0545

    setup cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    excludeSpecPattern: ["cypress/e2e/2-advanced-examples"],
+    //report
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      reportFilename: "[status]-[datetime]-[name]-report",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
 });
